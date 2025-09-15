54f9d7cd17e99f3fc806de9fed7214ae912b8b30
anadulimited@gmail.com <anadulimited@gmail.com>
2025-09-15 16:21:21 +0100

    Work on get one controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,38 +1,55 @@
-const userModel = require('../models/usermodel')
-const bcrypt = require('bcrypt')
+const userModel = require("../models/usermodel");
+const bcrypt = require("bcrypt");
 
-exports.register = async (req,res) => {
-    try {
-        const {fullName,email,password,age,phoneNumber} = req.body;
-        const existEmail = await userModel.findOne({email: email.toLowerCase()});
-        const existPhoneNumber = await userModel.findOne({phoneNumber: phoneNumber});
-        if(existEmail || existPhoneNumber){
-            return res.status(400).json({
-                messsage: 'User already exist'
-            })
-        };
+exports.register = async (req, res) => {
+  try {
+    const { fullName, email, password, age, phoneNumber } = req.body;
+    const existEmail = await userModel.findOne({ email: email.toLowerCase() });
+    const existPhoneNumber = await userModel.findOne({
+      phoneNumber: phoneNumber,
+    });
+    if (existEmail || existPhoneNumber) {
+      return res.status(400).json({
+        messsage: "User already exist",
+      });
+    }
     const saltRound = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(password, saltRound);
 
     const user = new userModel({
-        fullName,
-        email,
-        password: hashPassword,
-        age,
-        phoneNumber
+      fullName,
+      email,
+      password: hashPassword,
+      age,
+      phoneNumber,
     });
-    
-//await user.save();
+
+    //await user.save();
     res.status(201).json({
-        statusCode: true,
-        statusText: `Created`,
-        message: `User created successfully`,
-        data: user
-    })
-    } catch (error) {
-     res.status(500).json({
-        messsage:'Internal server error',
-        error:error.message
-     });
+      statusCode: true,
+      statusText: `Created`,
+      message: `User created successfully`,
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      messsage: "Internal server error",
+      error: error.message,
+    });
+  }
+};
+
+exports.getOne = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await userModel.findById(id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-}
+    res.status(200).json({ message: "User found", data: user });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+};
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,7 +1,8 @@
-const {register} = require('../controllers/userController')
+const {register, getOne} = require('../controllers/userController')
 const uploads = require('../middleware/multer')
 const router = require('express').Router();
 
 router.post('/register',uploads.single('profilePicture'), register)
+router.get('/getOne/:id',getOne)
 
 module.exports = router
